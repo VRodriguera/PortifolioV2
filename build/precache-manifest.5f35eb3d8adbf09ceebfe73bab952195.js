71d8a450efa686c7baebd708b5c5d316f76e2766
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-08-19 18:56:24 -0300

    Build do portifolio

---
+++ build/precache-manifest.5f35eb3d8adbf09ceebfe73bab952195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549ab03f202ca2ce51d1fb898663076c",
+    "url": "/PortifolioV2/index.html"
+  },
+  {
+    "revision": "d0c7c9bdcf0f013666b1",
+    "url": "/PortifolioV2/static/css/main.32b25d8d.chunk.css"
+  },
+  {
+    "revision": "8872d6f535243a3527a8",
+    "url": "/PortifolioV2/static/js/2.133e628d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/PortifolioV2/static/js/2.133e628d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c7c9bdcf0f013666b1",
+    "url": "/PortifolioV2/static/js/main.39139f70.chunk.js"
+  },
+  {
+    "revision": "bc19916a4c5c925ded08",
+    "url": "/PortifolioV2/static/js/runtime-main.7c457999.js"
+  },
+  {
+    "revision": "f0b922e9a4a41b3a0760ea100d4274ed",
+    "url": "/PortifolioV2/static/media/FotoPerfil.f0b922e9.jpeg"
+  }
+]);
